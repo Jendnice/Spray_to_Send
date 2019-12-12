30b8362bc7ee0258692395e4202d6711098d3e90
Jendnice <Pipeline38@hotmail.com>
2019-12-12 11:42:13 -0500

    remove unnecessary code in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,8 +6,6 @@ class Review < ApplicationRecord
     validates :stars, presence: true
     validates :title, presence: true
     validates :content, presence: true
-    
-    # validates :climb_id, presence: true
     validates :user_id, presence: true 
     
 end
